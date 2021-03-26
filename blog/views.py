9ad3d5e5991861451fcf6c3e49fcb42c0a3ad39b
Ha-moon <hyemin9974@gmail.com>
2021-03-26 17:22:11 +0900

    DB에서 목록 템플릿 가져와서 보여주기 Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # 쿼리셋을 담은 변수posts 생성.(쿼리셋의 이름붙여줌)
+    return render(request, 'blog/post_list.html', {'posts':posts})
