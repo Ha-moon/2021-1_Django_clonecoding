6e84530aeb6756428e23beec6064b025ba948758
Ha-moon <hyemin9974@gmail.com>
2021-03-27 17:42:24 +0900

    폼 저장하기

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -13,5 +13,14 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST": # 폼에 입력된 데이터를 view 페이지로 가지고 올 때(뭐 쓴 후 저장할때)
+        form = PostForm(request.POST)
+        if form.is_valid(): # 폼에 들어있는 값들이 올바른지 확인
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk) # 새로 작성한 글 페이지로 넘어감
+    else: # 처음 페이지에 접속했을 때
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
