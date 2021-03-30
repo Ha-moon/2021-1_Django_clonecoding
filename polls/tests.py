e4f1ba9318fac461af97fc97efc93d47671754a4
Ha-moon <hyemin9974@gmail.com>
2021-03-30 21:46:37 +0900

    part5-테스트 작성해보기-문제를 인지하고,오류등을 예방

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False를 반환한다 for questions whose pub_date가 미래일때,
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30) #  pub_date필드가 30일 이후(미래)인
+        future_question = Question(pub_date=time) # Question 인스턴스를 생성
+        self.assertIs(future_question.was_published_recently(), False)
