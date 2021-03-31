e98fe2c483fe82dc8d8985bfa9df656a65869e7c
Ha-moon <hyemin9974@gmail.com>
2021-03-31 17:14:25 +0900

    part7-관리자 화면 커스터마이징

--- polls/admin.py
+++ polls/admin.py
@@ -12,7 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'pub_date', 'was_published_recently') #리스트에 보이는 것들 커스텀
     list_filter = ['pub_date'] # 오른쪽에 filter 사이드바 추가
     search_fields = ['question_text'] # 맨 위에 검색창 추가
 
