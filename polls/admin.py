bec56d1fad9486b7b22661f852e1017ca4f9084c
Ha-moon <hyemin9974@gmail.com>
2021-03-31 16:49:56 +0900

    part7-Choice항목들 보이는 형태 커스터마이징

--- polls/admin.py
+++ polls/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline): # StackedInline/TabularInline형태옵션
+    model = Choice
+    extra = 3  # 기본으로 3가지 선택 항목을 제공
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
