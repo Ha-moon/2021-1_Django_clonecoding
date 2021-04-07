02f6a72111cea740f7259326456ac0888cc47dc5
Ha-moon <hyemin9974@gmail.com>
2021-04-07 17:50:02 +0900

    깃허브 초록색 연결 테스트

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/  
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '86_#ioxw)21%ve1xsq4uzi!@c*c0jxpg6bnee#m7y46a+$vivc'
