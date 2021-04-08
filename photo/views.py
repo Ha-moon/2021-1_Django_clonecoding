017f276c0d8fe8cf170f706242dd6d9339cf367f
Ha-moon <hyemin9974@gmail.com>
2021-04-08 14:02:52 +0900

    (9~10)권한문제 해결하기-2차-views.py에서 해결

--- photo/views.py
+++ photo/views.py
@@ -4,6 +4,8 @@ from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Photo
 
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 class PhotoList(ListView):
     model = Photo
@@ -27,14 +29,30 @@ class PhotoCreate(CreateView):
 
 class PhotoUpdate(UpdateView):
     model = Photo
-    fields = ['text', 'image']
+    fields = ['author', 'text', 'image']
     template_name_suffix = '_update'
     success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, '수정할 권한이 없습니다')
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoUpdate, self).dispatch(request, *args, **kwargs)
 
 class PhotoDelete(DeleteView):
     model = Photo
     template_name_suffix = '_delete'
     success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, '삭제할 권한이 없습니다')
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoDelete, self).dispatch(request, *args, **kwargs)    
 
 class PhotoDetail(DetailView):
     model = Photo
